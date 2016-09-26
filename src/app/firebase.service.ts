c3f37ac64a0816585de81a8630d61ef6453c5cbe
NIrving <NIrving>
2016-09-26 10:25:35 +1000

    Updated Firebase Data Model

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -2,17 +2,19 @@ import { Injectable } from '@angular/core';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 import { Auth } from './auth.service';
+import { FirebaseData } from './firebase.model';
+
 
 @Injectable()
 export class FirebaseService {
 
-    items: FirebaseListObservable<any>;
+    items: FirebaseListObservable<FirebaseData[]>;
 
     constructor(private auth: Auth, private af: AngularFire) {
         this.af.auth.subscribe(auth => {
             if (auth) {
                 console.log('logged in');
-                this.items = af.database.list('/stats');
+                this.items = af.database.list('/');
             } else {
                 console.log('not logged in');
                 this.items = null;
